8271cf2190c191ac3a9e61cacdfc83c239028c07
Tu Nombre <tuemail@example.com>
2026-07-11 23:45:53 -0400

    caray

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,23 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector("nav ul");
+const header = document.querySelector("header");
+
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+    header.classList.toggle("open");
+
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "X";
+    } else {
+        menuButton.textContent = "☰";
+    }
+
+});
