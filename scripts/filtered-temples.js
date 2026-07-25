6941aa697fab5a607ccdc755e1e264238b9d8ae9
Tu Nombre <tuemail@example.com>
2026-07-25 19:41:12 -0400

    auch

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,177 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector("nav ul");
+const header = document.querySelector("header");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    header.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "X";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
+
+const temples = [
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+  },
+  {
+    templeName: "Santiago Chile",
+    location: "Santiago, Chile",
+    dedicated: "1983, September, 15",
+    area: 20743,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    templeName: "Concepción Chile",
+    location: "Concepción, Chile",
+    dedicated: "2018, October, 28",
+    area: 36000,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    templeName: "Antofagasta Chile",
+    location: "Antofagasta, Chile",
+    dedicated: "2022, June, 16",
+    area: 26000,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  }
+];
+
+const gallery = document.querySelector(".gallery");
+const home = document.querySelector("#home");
+const old = document.querySelector("#old");
+const newTemple = document.querySelector("#new");
+const large = document.querySelector("#large");
+const small = document.querySelector("#small");
+
+
+function displayTemples(templeList) {
+    gallery.innerHTML = "";
+
+    templeList.forEach((temple) => {
+
+        const card = document.createElement("section");
+        const name = document.createElement("h2");
+        const location = document.createElement("p");
+        const dedicated = document.createElement("p");
+        const area = document.createElement("p");
+        const image = document.createElement("img");
+
+        name.textContent = temple.templeName;
+        location.innerHTML = `<strong>Location:</strong> ${temple.location}`;
+        dedicated.innerHTML = `<strong>Dedicated:</strong> ${temple.dedicated}`;
+        area.innerHTML = `<strong>Area:</strong> ${temple.area} sq ft`;
+
+        image.src = temple.imageUrl;
+        image.alt = temple.templeName;
+        image.loading = "lazy";
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedicated);
+        card.appendChild(area);
+        card.appendChild(image);
+
+        gallery.appendChild(card);
+    });
+}
+
+displayTemples(temples);
+
+home.addEventListener("click", () => {
+    displayTemples(temples);
+});
+old.addEventListener("click", () => {
+    const oldTemples = temples.filter((temple) => {
+        return new Date(temple.dedicated).getFullYear() < 1900;
+    });
+
+    displayTemples(oldTemples);
+});
+
+newTemple.addEventListener("click", () => {
+    const newTemples = temples.filter((temple) => {
+        return new Date(temple.dedicated).getFullYear() > 2000;
+    });
+
+    displayTemples(newTemples);
+});
+
+large.addEventListener("click", () => {
+    const largeTemples = temples.filter((temple) => {
+        return temple.area > 90000;
+    });
+
+    displayTemples(largeTemples);
+});
+
+small.addEventListener("click", () => {
+    const smallTemples = temples.filter((temple) => {
+        return temple.area < 10000;
+    });
+
+    displayTemples(smallTemples);
+});
---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,3 @@
+const modified = document.querySelector("#lastModified");
+
+modified.textContent = document.lastModified;
